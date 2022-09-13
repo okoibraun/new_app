550f13ed01623b78e1de415a7d16c60a3ebb2fa0
okoibraun <okoibraun@gmail.com>
2022-09-13 12:50:40 +0100

    added html

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
+
 echo "Hello World";
 
-echo "this line appears only when we are in our dev branch";
+echo "this line appears only when we are in our dev branch";?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> git</title>
+</head>
+<body>
+    <h1>cloning</h1>
+    <p>this is interesting</p>
+</body>
+</html>
